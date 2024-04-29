8a7252e94018afc9cb37d2bba3880fccaae3d487
Chidiebere Onyegbuchulem <chidiebere@bepeerless.co>
2024-04-29 03:31:15 +0100

    feat: create search and icon components

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useState } from 'react';
+import { Icon } from './Icon';
+
+interface Props {
+  placeholder?: string;
+}
+
+export const SearchBar = () => {
+  const [searchItem, setSearchItem] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setSearchItem(value);
+  };
+
+  return (
+    <div className="relative flex items-center">
+      <button type="button" className="absolute left-2">
+        <Icon name="Search" className="w-6 h-6 " />
+      </button>
+      <input
+        type="search"
+        className="py-2 pl-8 pr-3 w-full bg-gray-100 rounded focus:outline-none focus:none"
+        value={searchItem}
+        onChange={handleInputChange}
+        placeholder="Search"
+      />
+    </div>
+  );
+};
